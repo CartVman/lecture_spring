1d3244902517427d77475c81d06ac52377596d5b
CartVman <kurtvalderama11@gmail.com>
2022-02-12 01:48:48 +0100

    Initial commit

---
+++ src/main/java/oslomet/webprog/lecture_spring/LectureSpringApplication.java
@@ -0,0 +1,13 @@
+package oslomet.webprog.lecture_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LectureSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/oslomet/webprog/lecture_spring/LectureSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package oslomet.webprog.lecture_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LectureSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
